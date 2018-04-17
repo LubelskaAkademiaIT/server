b78c5d8ee57e4ab93bd84ed4ada5832b6ee56fd5
UNIVERSE\msikora <maciej.sikora@dataart.com>
2018-04-17 15:16:28 +0200

    allow cors

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json()); // for parsing application/json
 
+// allow cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.all('*', (req, res, next) => {
   setTimeout(next, config.delay); //delay for all responses
 });
